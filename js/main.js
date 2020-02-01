d9de820813b63c1d38d3e8f541118919ac77b347
James Holt <james.holt@hopeforjustice.org>
2020-02-01 17:13:23 +0000

    adding more location points

--- js/main.js
+++ js/main.js
@@ -1,8 +1,54 @@
   $(document).ready(function(){
     $("#1").click(function(){
-      $(".modal").fadeIn();
+      $(".modal1").fadeIn();
     });
+
+    $("#2").click(function(){
+      $(".modal2").fadeIn();
+    });
+
+    $("#3").click(function(){
+      $(".modal3").fadeIn();
+    });
+
+    $("#4").click(function(){
+      $(".modal4").fadeIn();
+    });
+
+    $("#5").click(function(){
+      $(".modal5").fadeIn();
+    });
+
+    $("#6").click(function(){
+      $(".modal6").fadeIn();
+    });
+
+    $("#7").click(function(){
+      $(".modal7").fadeIn();
+    });
+
+    $("#8").click(function(){
+      $(".modal8").fadeIn();
+    });
+
+    $("#9").click(function(){
+      $(".modal9").fadeIn();
+    });
+
+    $("#10").click(function(){
+      $(".modal10").fadeIn();
+    });
+
+    $("#11").click(function(){
+      $(".modal11").fadeIn();
+    });
+
+    $("#12").click(function(){
+      $(".modal12").fadeIn();
+    });
+
     $(".close").click(function(){
       $(".modal").fadeOut();
     });
+
   });
